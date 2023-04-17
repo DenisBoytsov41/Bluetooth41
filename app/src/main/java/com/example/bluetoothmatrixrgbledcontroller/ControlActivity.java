fa5312f7e27c5fb4c9b029896b26c9cee3fc7466
stud <goodpromised41@gmail.com>
2023-04-17 10:48:09 +0300

    --------

--- app/src/main/java/com/example/bluetoothmatrixrgbledcontroller/ControlActivity.java
+++ app/src/main/java/com/example/bluetoothmatrixrgbledcontroller/ControlActivity.java
@@ -6,9 +6,11 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -43,6 +45,8 @@ public class ControlActivity extends AppCompatActivity {
         TextView textViewR = findViewById(R.id.seekBarValueR);
         TextView textViewG = findViewById(R.id.seekBarValueG);
         TextView textViewB = findViewById(R.id.seekBarValueB);
+        Button colBut = findViewById(R.id.button);
+
         xSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -128,6 +132,22 @@ public class ControlActivity extends AppCompatActivity {
 
             }
         });
+        SeekBar.OnSeekBarChangeListener seeekchange= new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                colBut.setBackgroundColor(Color.rgb(rSeekBar.getProgress(),gSeekBar.getProgress(),bSeekBar.getProgress()));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        };
 
         mac = getIntent().getStringExtra("mac");
 
